b00764e65d879efc42f7105ac3f2902698d7a1ed
Yannick Weiss <yannick.weiss@goldbachinteractive.ch>
2014-10-28 08:58:35 +0100

    Initial Commit

---
+++ GBIWallpaper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GBIWallpaper
+//
+//  Created by Yannick Weiss on 28.10.14.
+//  Copyright (c) 2014 GBI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
