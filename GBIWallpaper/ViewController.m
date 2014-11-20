e57c7429916c41bef086fdc0dfc9db6d24a0fbb1
Yannick Weiss <yannick.weiss@goldbachinteractive.ch>
2014-11-20 17:54:13 +0100

    added dynamic fields

--- GBIWallpaper/ViewController.m
+++ GBIWallpaper/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *devicename;
+@property (weak, nonatomic) IBOutlet UILabel *devicemodel;
+@property (weak, nonatomic) IBOutlet UILabel *deviceversion;
+@property (weak, nonatomic) IBOutlet UILabel *deviceresolution;
 
 @end
 
@@ -17,6 +21,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.devicename.text = [[UIDevice currentDevice] name];
+    self.devicemodel.text = [[[UIDevice currentDevice] model] stringByAppendingString:@"12345"];
+    self.deviceversion.text = [@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]];
 }
 
 - (void)didReceiveMemoryWarning {
