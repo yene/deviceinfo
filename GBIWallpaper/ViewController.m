feb759d9b8324e7645d678476eddb82b16e06d6c
Yannick Weiss <yannick.weiss@goldbachinteractive.ch>
2014-11-20 18:14:11 +0100

    improved displayed information

--- GBIWallpaper/ViewController.m
+++ GBIWallpaper/ViewController.m
@@ -10,7 +10,6 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *devicename;
-@property (weak, nonatomic) IBOutlet UILabel *devicemodel;
 @property (weak, nonatomic) IBOutlet UILabel *deviceversion;
 @property (weak, nonatomic) IBOutlet UILabel *deviceresolution;
 
@@ -22,12 +21,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.devicename.text = [[UIDevice currentDevice] name];
-    self.devicemodel.text = [[[UIDevice currentDevice] model] stringByAppendingString:@"12345"];
     self.deviceversion.text = [@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]];
     CGRect screenBounds = [[UIScreen mainScreen] bounds];
     CGFloat screenScale = [[UIScreen mainScreen] scale];
     NSString *res = screenScale > 1 ? @"Retina " : @"";
-    res = [NSString stringWithFormat:@"%@ (%dx%d)", res, (int)floor(screenBounds.size.height), (int)floor(screenBounds.size.width * screenScale)];
+    res = [NSString stringWithFormat:@"%@%dx%d", res, (int)floor(screenBounds.size.height), (int)floor(screenBounds.size.width * screenScale)];
     self.deviceresolution.text = res;
 }
 
